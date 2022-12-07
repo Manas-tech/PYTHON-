409fdf4c3a2ec286631caee07c646f8610888a52
manas parwani _TECHNONJR <115521778+Manas-tech@users.noreply.github.com>
2022-12-07 21:00:35 +0530

    Add files via upload

---
+++ notf.py
@@ -0,0 +1,15 @@
+import time
+from plyer import notification
+
+
+if __name__=="__main__":
+
+		notification.notify(
+			title = "HEY MANAS !!!!!!!",
+			message="What you seek is seeking you " ,
+		
+			# displaying time
+			timeout=2
+)
+		# waiting time
+		time.sleep(5)
