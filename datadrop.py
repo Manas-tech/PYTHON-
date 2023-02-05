b373e86b036654f4d2511dff113d47df4936c9ab
Manas-tech <115521778+Manas-tech@users.noreply.github.com>
2023-02-05 15:54:56 +0530

    Add files via upload

---
+++ datadrop.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+d={"age":[1,2,3],"salary":[122,133,144]}
+print(type(d))
+data =pd.DataFrame(d)
+data.loc[3]=[4,155]
+data.drop(columns=['salary'],axis=1,inplace=True)
+print(data)
