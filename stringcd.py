b7f369a553246993019e84f6e8fafe5c2e40c0fb
Manas-tech <115521778+Manas-tech@users.noreply.github.com>
2023-05-03 21:56:40 +0530

    Update stringcd.py

--- stringcd.py
+++ stringcd.py
@@ -1,7 +1,7 @@
 myStr = "pyctchonfcorbegcinncers"
 print("The original string is:", myStr)
 charToDelete = 'p'
-print("The character to delete:", charToDelete)
+print("The character to deleted:", charToDelete)
 myList = [character for character in myStr if character != charToDelete]
 print("The list of characters is:")
 print(myList)
