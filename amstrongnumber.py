36a6937a5f8721bc63a32aa6b4338b47356597f2
Manas-tech <115521778+Manas-tech@users.noreply.github.com>
2023-08-01 15:13:34 +0530

    Update amstrongnumber.py

--- amstrongnumber.py
+++ amstrongnumber.py
@@ -1,6 +1,6 @@
 # Python program to check if the number is an Armstrong number or not
 
-# take input from the user
+# take input from  the user
 num = int(input("Enter a number :  "))
 
 # initialize sum
